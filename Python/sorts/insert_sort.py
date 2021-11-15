a329b3f7dd8d44ce072ea17cd22122fd92166b18
fanxiangs <1378522044@qq.com>
2021-11-15 11:12:37 +0800

    python: add insert_sort

---
+++ Python/sorts/insert_sort.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def insert_sort(nums):
+    """
+    选择排序:
+        构建有序数组，对未排序数据，从后向前扫描找到对应位置插入
+    时间复杂度:
+        O(n^2)
+    @param nums: 无序数组
+    @return: 升序数组
+    >>> import random
+    >>> nums = random.sample(range(-50, 50), 50)
+    >>> insert_sort(nums) == sorted(nums)
+    True
+    >>> insert_sort([0, 5, 2, 3, 2]) == sorted([0, 5, 2, 3, 2])
+    True
+    >>> insert_sort([])
+    []
+    """
+    length = len(nums)
+    for i in range(1, length):
+        for j in range(i, 0, -1):
+            if nums[j] < nums[j - 1]:
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+    return nums
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
